0528314e05e13ad7a89adc1ca601b7bfb57cfc9d
Allan Ebdrup <allan@878.dk>
2012-06-26 08:46:41 +0200

    jsHint updates

--- bin/runAllTests.js
+++ bin/runAllTests.js
@@ -1,21 +1,27 @@
+//use strict
 var spawn = require('child_process').spawn;
-var jsHint = spawn("node", ["./node_modules/jshint/bin/hint","./lib"]);
+var jsHint = spawn("node", ["./node_modules/jshint/bin/hint", "./lib"]);
 var errors = false;
-jsHint.stdout.on("data", function(data){
+jsHint.stdout.on("data", function (data) {
 	console.log('jsHint: ' + data);
 });
-jsHint.stderr.on("data", function(data){
+jsHint.stderr.on("data", function (data) {
 	console.error('jsHint error: ' + data);
 	errors = true;
 });
-if(errors){
-	process.exit(1);
-}
-var realExit = process.exit;
-process.exit = function(){
-	var args = arguments;
-	setTimeout(function(){
-		realExit.apply(process, args);
-	}, 1000); //for some reason Mocha does not flush data output before exiting the process, this compensates
-}
-require('../node_modules/mocha/bin/_mocha');
+
+jsHint.on("exit", function () {
+	//use strict
+	if (errors) {
+		process.exit(1);
+	}
+	//run mocha
+	var realExit = process.exit;
+	process.exit = function () {
+		var args = arguments;
+		setTimeout(function () {
+			realExit.apply(process, args);
+		}, 1000); //for some reason Mocha does not flush data output before exiting the process, this compensates
+	};
+	require('../node_modules/mocha/bin/_mocha');
+});
--- test/unit/globals.spec.js
+++ test/unit/globals.spec.js
@@ -1,23 +1,23 @@
-describe("It has globals defined for testing", function(){
+describe("Global variables for testing", function () {
 
-	it("has sinon defined", function(){
-		expect(sinon).to.be.ok
+	it("has sinon defined", function () {
+		expect(sinon).to.be.ok;
 	});
 
-	it("has expect defined", function(){
-		expect(expect).to.be.ok
+	it("has expect defined", function () {
+		expect(expect).to.be.ok;
 	});
 
-	it("has injectr defined", function(){
-		expect(injectr).to.be.ok
+	it("has injectr defined", function () {
+		expect(injectr).to.be.ok;
 	});
 
-	it("has underscore defined", function(){
-		expect(_).to.be.ok
+	it("has underscore defined", function () {
+		expect(_).to.be.ok;
 	});
 
-	it("has request defined", function(){
-		expect(request).to.be.ok
+	it("has request defined", function () {
+		expect(request).to.be.ok;
 	});
 
 });
