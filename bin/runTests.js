d6b09dcf81538a90e9ab216de50fd308aa15b7b9
Allan Ebdrup <allan@878.dk>
2012-07-15 20:30:01 +0200

    better way of flushing output before process.exit

--- bin/runTests.js
+++ bin/runTests.js
@@ -2,6 +2,14 @@
 var fs = require("fs");
 var path = require("path");
 
+//make writes to stdout and stderr sync, so the process does not exit before writing output.
+process.stdout.write = function (data){
+	fs.writeSync(1, data);
+};
+process.stderr.write = function (data){
+	fs.writeSync(2, data);
+};
+
 runJsHint(["./lib", "./test", "./bin"], function (err) {
 	if (err) {
 		console.error("Exiting because of jsHint errors");
@@ -12,16 +20,7 @@ runJsHint(["./lib", "./test", "./bin"], function (err) {
 });
 
 function runMocha() {
-	var realExit = process.exit;
-	process.exit = function () {
-		var args = arguments;
-		//stdout does not flush data output before exiting the process, this compensates
-		process.stdout.once("drain", function (){
-			setTimeout(function(){
-				realExit.apply(process, args);
-			}, 500);
-		});
-	};
+
 	require('../node_modules/mocha/bin/_mocha');
 }
 
@@ -39,10 +38,7 @@ function runJsHint(pathsArray, callback) {
 		});
 	var results = jsHint.hint(pathsArray, config, reporter, ignores);
 	if (results.length > 0) {
-		//stdout does not flush data output before exiting the process, this compensates
-		return process.stdout.once("drain", function () {
-			return callback(new Error("JSHintErrors"));
-		});
+		return callback(new Error("JSHintErrors"));
 	}
 	return callback(null);
 }
