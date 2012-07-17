05e65e74381595bd358a8888346dbbf6774ce019
Allan Ebdrup <allan@878.dk>
2012-07-17 08:24:10 +0200

    update with new nodecoverage feature.

--- bin/runCoverage.js
+++ bin/runCoverage.js
@@ -2,10 +2,11 @@
 process.env.NODE_ENV = "coverage";
 var nodecoverage = require("nodecoverage");
 nodecoverage({
-	instrument:["lib"]
+	instrument:["lib"],
+	requireBeforeRun:["lib/app.js"] //used for starting up a http server or similar, before running all tests
 }, function (err) {
 	if(err){
 		console.error(err);
 	}
-	//process.exit();
+	process.exit();
 });
--- test/integration/testModule.spec.js
+++ test/integration/testModule.spec.js
@@ -3,7 +3,8 @@ describe("When running app.js", function () {
 
 	it("runs without throwing an error", function () {
 		//just a dummy test to get nicer code coverage report, when I'm testing
-		var app = require("../../lib/app.js");
-		expect(app).to.be.ok;
+		var testModule = require("../../lib/testModule.js");
+		var result = testModule();
+		expect(result).to.equal("testModule.js was tested");
 	});
 });
